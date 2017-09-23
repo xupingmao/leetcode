b293d75a05fffcfca2a4dba8e27e7e39f0f3bcde
xupingmao <578749341@qq.com>
2017-09-23 17:59:53 +0800

    Update

--- shell/192-word-frequency.sh
+++ shell/192-word-frequency.sh
@@ -1,8 +1,7 @@
-
 # Read from the file words.txt and output the word frequency list to stdout.
 
-# tr 用来分隔字符串
-# tr -d string1 使用string1分隔，并且删除string1
-# tr -s string1 
+# tr 用来替换字符串
+# tr -d string1 删除string1
+# tr -s string1 string2 把string1替换成string2
 cat words.txt | tr -s ' ' '\n' | sort | uniq -c | sort -r | awk '{print $2, $1}'
 
